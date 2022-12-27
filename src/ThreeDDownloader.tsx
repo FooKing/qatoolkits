2ce2a331c6e316c9fcc8fb47f2c71b308834d328
FooKing <skaged@live.co.uk>
2022-12-27 19:13:19 +0000

    3dDownloaderOverhaul

--- src/ThreeDDownloader.tsx
+++ src/ThreeDDownloader.tsx
@@ -1,99 +1,147 @@
-import {
-    Box,
-    Button,
-    FormControl, Input,
-    InputLabel,
-    MenuItem,
-    Select, SelectChangeEvent,
-} from "@mui/material";
-import React, {useState} from "react";
-import {browser,} from "webextension-polyfill-ts";
+import {Box,Button,FormControl,Input,InputLabel,MenuItem,Select,SelectChangeEvent}
+    from "@mui/material";
+import React, {useState}
+    from "react";
+import {browser,}
+    from "webextension-polyfill-ts";
 
 
 function ThreeDDownloader() {
-
-    const [currentVersion, setCurrentVersion] = useState('');
+    const [isDownloading, setIsDownloading] = useState(false);
+    const [isLatest, setIsLatest] = useState(true);
+    const [customVersion, setCustomVersion] = useState('');
     const [environment, setEnvironment] = useState('');
-    const handleVersionChange = (e:React.ChangeEvent<HTMLInputElement>) => setCurrentVersion(e.currentTarget.value);
-    const handleEnvironmentChange = (e:SelectChangeEvent) => setEnvironment(e.target.value);
+    const [staticLocation, setStaticLocation] = useState('static');
+    const [currentPlatform, setCurrentPlatform] = useState('gameCI');
+    const handleVersionChange = (e:React.ChangeEvent<HTMLInputElement>) => setCustomVersion(e.target.value);
+    const handleEnvironmentChange = (e:SelectChangeEvent) => {
+        setEnvironment(e.target.value);
+        const currentEnvironment = enviroArray.find(env => env.Code === e.target.value);
+        if (currentEnvironment) {
+            setStaticLocation(currentEnvironment.Static);
+        }
+
+    };
     let [userFeedbackMessage, setUserFeedbackMessage] = useState('');
 
-    const environmentArray = [
-        {Code: "Https://static.wrenkitchens.com/planner3d/gameCI/"+currentVersion+"/Planner3D.zip", Name: "Live"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project0/"+currentVersion+"/Planner3D.zip", Name: "Project 0"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project1/"+currentVersion+"/Planner3D.zip", Name: "Project 1"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project2/"+currentVersion+"/Planner3D.zip", Name: "Project 2"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project3/"+currentVersion+"/Planner3D.zip", Name: "Project 3"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project4/"+currentVersion+"/Planner3D.zip", Name: "Project 4"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project5/"+currentVersion+"/Planner3D.zip", Name: "Project 5"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project6/"+currentVersion+"/Planner3D.zip", Name: "Project 6"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project7/"+currentVersion+"/Planner3D.zip", Name: "Project 7"},
-        {Code: "Https://project-static.wrenkitchens.com/planner3d/gameCI/project8/"+currentVersion+"/Planner3D.zip", Name: "Project 8"}
+    const enviroArray = [
+        {Code: " ", Name: "Live", Static: "static"},
+        {Code: "project0/", Name: "Project 0", Static: "project-static"},
+        {Code: "project1/", Name: "Project 1", Static: "project-static"},
+        {Code: "project2/", Name: "Project 2", Static: "project-static"},
+        {Code: "project3/", Name: "Project 3", Static: "project-static"},
+        {Code: "project4/", Name: "Project 4", Static: "project-static"},
+        {Code: "project5/", Name: "Project 5", Static: "project-static"},
+        {Code: "project6/", Name: "Project 6", Static: "project-static"},
+        {Code: "project7/", Name: "Project 7", Static: "project-static"},
+        {Code: "project8/", Name: "Project 8", Static: "project-static"}
 
     ];
 
-    async function startUnzip(unzipDownloadItem: number) {
-        const downloads = await browser.downloads.search({id: (unzipDownloadItem)});
-        const download = downloads[0];
-        console.log(download.filename)
+    const handleDownloadButton = async () => {
+        let currentDownloadUrl = "";
+        if (isLatest) {
+            const requestUrl = new XMLHttpRequest();
+            const versionCheckUrl = `https://${staticLocation}.wrenkitchens.com/planner3d/gameCI/${environment}version.txt`.replace(/\s+/g,'');
+            requestUrl.open('GET', versionCheckUrl);
+            requestUrl.send();
+            requestUrl.onload = async function () {
+                if (requestUrl.status === 200) {
+                    const latestVersion = requestUrl.responseText;
+                    currentDownloadUrl = `https://${staticLocation}.wrenkitchens.com/planner3d/${currentPlatform}/${environment}${latestVersion}/Planner3D.zip`.replace(/\s+/g,'');
+                    await handleDownload(currentDownloadUrl, "Planner3D.zip");
+                } else {
+                    console.error(`Error ${requestUrl.status}: ${requestUrl.statusText}`);
+                }
+
+            }}
+        else
+        {
+            currentDownloadUrl = `https://${staticLocation}.wrenkitchens.com/planner3d/${currentPlatform}/${environment}${customVersion}/Planner3D.zip`.replace(/\s+/g,'');
+            await handleDownload(currentDownloadUrl, "Planner3D.zip");
+        }
     }
 
+    const handleDownloadManifestButton = async () => {
+        const manifestUrl = `https://${staticLocation}.wrenkitchens.com/planner3d/gameCI/${environment}manifest.json`.replace(/\s+/g,'');
+        await handleDownload(manifestUrl, 'manifest.json')
+       };
+
+    const handleLatestChange = (e: SelectChangeEvent) => {
+        setIsLatest(e.target.value === 'true');
+    };
+
+    const handlePlatformChange = (e: SelectChangeEvent) => {
+        setCurrentPlatform(e.target.value);
+    };
+
+
+    const handleDownload = async (downloadUrl: string, saveAs: string) => {
+        setUserFeedbackMessage(`Download Started`);
+        await browser.downloads.download({
+            url: downloadUrl,
+            conflictAction: "overwrite",
+            filename: saveAs
+        }).then((downloadItem) => {
+            trackDownloadProgress(downloadItem)
+            setIsDownloading(true);
+        });
+    }
     async function trackDownloadProgress(downloadItem: number) {
-
-        // Setup looping check
         const id = setInterval(async () => {
-
-            // search for the download by the ID passed downloadItem
             const downloads = await browser.downloads.search({id: (downloadItem)});
-
-            // Get first download found that matches ID
             const download = downloads[0];
-
-            // Check current download state.
             switch (download.state) {
                 case 'complete':
                     setUserFeedbackMessage(`Download complete`);
                     clearInterval(id);
-                    await startUnzip(downloadItem);
+                    setIsDownloading(false);
                     break;
                 case 'interrupted':
                     setUserFeedbackMessage(`Download failed`);
                     clearInterval(id);
+                    setIsDownloading(false);
                     break;
                 case 'in_progress':
                     let downloadPercent = Math.round((download.bytesReceived / download.totalBytes) * 100);
                     setUserFeedbackMessage(`Downloading ${downloadPercent}% complete`);
                     break;
             }
-            //Interval between loops
         }, 500);
     }
-    async function handleDownloadButton() {
-        if ((environment ?? null) && (currentVersion ?? null)) {
-            let currentUrl = environment;
-            setUserFeedbackMessage(`Download Started`);
-            await browser.downloads.download({url: currentUrl,conflictAction: "overwrite", filename: 'Planner3D.zip'}).then((downloadItem) => {
-                trackDownloadProgress(downloadItem)
-            });
-        }
-    }
+
     return (
-        <Box sx={{ flexGrow: 1, height: 100, width:300, borderRadius:1, border:1, padding:"5px"}}>
+        <Box title='3D downloader' sx={{ flexGrow: 1, height: 200, width:300, borderRadius:1, border:1, padding:"5px"}}>
             <FormControl sx={{ m: 1, width:90 }} variant="standard">
-                <InputLabel>Version</InputLabel>
-                <Input type= "number" value={currentVersion} onChange={handleVersionChange}></Input>
+                <InputLabel>Platform</InputLabel>
+                <Select size="small" id="LatestSelect" defaultValue={'gameCI'} onChange={handlePlatformChange}>
+                    <MenuItem value={'gameCI'}>Mac</MenuItem>
+                    <MenuItem value={'winCI'}>Win</MenuItem>
+                </Select>
             </FormControl>
             <FormControl sx={{ m: 1, width:90 }} variant="standard">
                 <InputLabel>Environment</InputLabel>
-                <Select size="small" id="projectSelect" value={environment} onChange={handleEnvironmentChange}
+                <Select size="small" id="projectSelect" value={environment} defaultValue={enviroArray[0].Name} onChange={handleEnvironmentChange}
                         MenuProps={{ PaperProps: { sx: { maxHeight: 200 } } }}>
-                    {environmentArray.map(({Code, Name},index ) => {
+                    {enviroArray.map(({Code, Name},index ) => {
                         return <MenuItem key={index} value={Code}>{Name}</MenuItem>
                     })}
                 </Select>
             </FormControl >
-                <Button  sx={{ m: 1, alignItems:"baseline"}} size="small" variant="contained" onClick={handleDownloadButton}> Download </Button>
-            <p>{userFeedbackMessage}</p>
+            <FormControl sx={{ m: 1, width:90 }} variant="standard">
+                <InputLabel>Version</InputLabel>
+                <Select size="small" id="LatestSelect" defaultValue={'true'} onChange={handleLatestChange}>
+                    <MenuItem value={'true'}>Latest</MenuItem>
+                    <MenuItem value={'false'}>Custom</MenuItem>
+                </Select>
+                </FormControl>
+            <FormControl sx={{ m: 1, width:90 }} variant="standard">
+                <InputLabel>Custom</InputLabel>
+                <Input type= "number" value={customVersion} disabled={isLatest} onChange={handleVersionChange}></Input>
+            </FormControl>
+                <Button  sx={{ m: 1, alignItems:"center"}} size="small" variant="contained" onClick={handleDownloadButton} disabled={isDownloading}> Download </Button>
+                <Button  sx={{ m: 1, alignItems:"center"}} size="small" variant="contained" onClick={handleDownloadManifestButton} disabled={isDownloading}> Manifest Download </Button>
+                <p>{userFeedbackMessage}</p>
         </Box>
     );
 }
